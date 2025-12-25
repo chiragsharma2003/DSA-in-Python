192f9a6f3fe7a3d77678cb56507329704ffc9d5d
CHIRAG SHARMA <105839229+chiragsharma2003@users.noreply.github.com>
2025-12-25 14:29:33 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,8 @@
+# 9. Palindrome Number
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
