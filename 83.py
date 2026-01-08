36381ae77e991fb25b5cf7070e76353b860d5948
CHIRAG SHARMA <105839229+chiragsharma2003@users.noreply.github.com>
2026-01-08 11:02:37 +0530

    Add files via upload

---
+++ 83.py
@@ -0,0 +1,19 @@
+# 83. Remove Duplicates from Sorted List
+# Given the head of a sorted linked list, delete all duplicates such that each
+# element appears only once. Return the linked list sorted as well.
+
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        
+        temp = head
+        # print(temp.next.val)
+        while temp and temp.next != None:
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
+
+             
+        
