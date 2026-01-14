50e5b7221052e420f4847184844fa219077b14eb
CHIRAG SHARMA <105839229+chiragsharma2003@users.noreply.github.com>
2026-01-14 15:53:09 +0530

    Add files via upload

---
+++ 101.py
@@ -0,0 +1,15 @@
+# 101. Symmetric Tree
+# Given the root of a binary tree, check whether it is a mirror of itself 
+# (i.e., symmetric around its center).
+
+class Solution(object):
+    def isSymmetric(self, root):
+        def same(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            return same(root1.left, root2.right) and same(root1.right, root2.left)
+        return same(root, root)
