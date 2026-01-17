a368318c20f5cfde0e883f222b8749e4609f0c5f
CHIRAG SHARMA <105839229+chiragsharma2003@users.noreply.github.com>
2026-01-17 12:57:36 +0530

    Add files via upload

---
+++ 110.py
@@ -0,0 +1,11 @@
+# 110. Balanced Binary Tree
+# Given a binary tree, determine if it is height-balanced.
+
+class Solution(object):
+    def isBalanced(self, root):
+        return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None:  return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+        return max(leftheight, rightheight) + 1
